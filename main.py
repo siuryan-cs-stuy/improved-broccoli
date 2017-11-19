2052ff7bfec1e1758f9ef77259a400fee8e2453c
Ryan Siu <rsiu@stuy.edu>
2017-11-19 12:19:17 -0500

    Finish base template

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = True
