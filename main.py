89aa03b077e8d774b0b6050476df2841fbe0172f
Ryan Siu <rsiu@stuy.edu>
2017-11-20 22:00:44 -0500

    Work on college profile template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-import api.py
+import api
+import config
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+    print config.GOOGLE_API_KEY
+    return render_template('profile.html', GOOGLE_API_KEY = config.GOOGLE_API_KEY)
 
 if __name__ == '__main__':
     app.debug = True
